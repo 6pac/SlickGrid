5e2261a6128026cbf133060c3e106d2208fc3de8
Muthukumar Selvarasu <muthukumar.selvarasu@anz.com>
2018-02-27 19:45:54 +0800

    removed setupColumnReorder on example updated to use setupColumnReorder from grid existing function with small update.
    Earlier it was used from example implementation
    
    updated example

--- slick.grid.js
+++ slick.grid.js
@@ -876,20 +876,24 @@ if (typeof Slick === "undefined") {
 
     function setupColumnReorder() {
       $headers.filter(":ui-sortable").sortable("destroy");
+      var $headerDraggableGroupBy = $(getPreHeaderPanel());
       $headers.sortable({
-        containment: "parent",
         distance: 3,
-        axis: "x",
         cursor: "default",
         tolerance: "intersection",
         helper: "clone",
         placeholder: "slick-sortable-placeholder ui-state-default slick-header-column",
+        forcePlaceholderSize: true,
+        appendTo: "body",
         start: function (e, ui) {
-          ui.placeholder.width(ui.helper.outerWidth() - headerColumnWidthDiff);
           $(ui.helper).addClass("slick-header-column-active");
+          $headerDraggableGroupBy.find(".slick-placeholder").show();
+          $headerDraggableGroupBy.find(".slick-dropped-grouping").hide();
         },
         beforeStop: function (e, ui) {
           $(ui.helper).removeClass("slick-header-column-active");
+          $headerDraggableGroupBy.find(".slick-placeholder").hide();
+          $headerDraggableGroupBy.find(".slick-dropped-grouping").show();
         },
         stop: function (e) {
           if (!getEditorLock().commitCurrentEdit()) {
