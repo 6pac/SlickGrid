0ac7716e1a2789b6e5e2a88fdb9003426abcdd37
ghiscoding <gbeaulac@gmail.com>
2019-02-26 09:10:47 -0500

    simplified checkbox selection formatter with override fn

--- plugins/slick.checkboxselectcolumn.js
+++ plugins/slick.checkboxselectcolumn.js
@@ -290,12 +290,8 @@
       var UID = createUID() + row;
 
       if (dataContext) {
-        if (typeof _selectableOverride === 'function') {
-          if (_selectableOverride(row, dataContext, grid)) {
-            return _selectedRowsLookup[row]
-                ? "<input id='selector" + UID + "' type='checkbox' checked='checked'><label for='selector" + UID + "'></label>"
-                : "<input id='selector" + UID + "' type='checkbox'><label for='selector" + UID + "'></label>";
-          }
+        if (!checkSelectableOverride(row, dataContext, grid)) {
+          return null;
         } else {
           return _selectedRowsLookup[row]
               ? "<input id='selector" + UID + "' type='checkbox' checked='checked'><label for='selector" + UID + "'></label>"
