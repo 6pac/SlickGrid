ee43ff5e721c20f70156fef2966d12429bb767e6
Nick Beckenham <solgarius@users.noreply.github.com>
2019-06-27 15:55:21 +0100

    add missing keycode file.

---
+++ keycode.js
@@ -0,0 +1,18 @@
+module.exports = {
+  BACKSPACE: 8,
+  COMMA: 188,
+  DELETE: 46,
+  DOWN: 40,
+  END: 35,
+  ENTER: 13,
+  ESCAPE: 27,
+  HOME: 36,
+  LEFT: 37,
+  PAGE_DOWN: 34,
+  PAGE_UP: 33,
+  PERIOD: 190,
+  RIGHT: 39,
+  SPACE: 32,
+  TAB: 9,
+  UP: 38
+}
