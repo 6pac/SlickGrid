1ec6033f4d6165bf80ec83ebbb8fe0bcb9e4932c
Ghislain Beaulac <ghislain.beaulac@se.com>
2019-12-09 19:04:49 -0500

    feat(cypress): add context menu cypress test for 1st row

--- cypress/integration/exampleContextMenu.spec.js
+++ cypress/integration/exampleContextMenu.spec.js
@@ -16,4 +16,82 @@ describe('Example - Context Menu & Cell Menu', () => {
       .children()
       .each(($child, index) => expect($child.text()).to.eq(fullTitles[index]));
   });
+
+  it('should have first row with "Task 0" and a Priority set to "Low" with the Action cell disabled and not clickable', () => {
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(1)')
+      .contains("Task 0");
+
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(5)')
+      .contains("Low");
+
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(6) img')
+      .invoke('attr', 'src')
+      .then(src => {
+        expect(src).to.contain('tick.png')
+      });
+
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(7) .disabled')
+      .contains("Action");
+
+    cy.get('.slick-cell-menu')
+      .should('not.exist')
+  });
+
+  it('should change "Task 0" Priority to "High" and expect the Action to become clickable and usable', () => {
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(1)')
+      .contains("Task 0");
+
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(5)')
+      .rightclick();
+
+    cy.get('.slick-context-menu .slick-context-menu-option-list')
+      .contains("High")
+      .click();
+
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(7)')
+      .contains("Action")
+      .click();
+
+    cy.get('.slick-cell-menu')
+      .should('exist');
+  });
+
+  it('should expect a "Command 2" to be disabled and not clickable (menu will remain open), in that same Action menu', () => {
+    cy.get('.slick-cell-menu .slick-cell-menu-item.slick-cell-menu-item-disabled')
+      .contains("Command 2")
+      .click();
+
+    cy.get('.slick-cell-menu')
+      .should('exist');
+  });
+
+  it('should change the Effort Driven to "False" in that same Action and then expect the "Command 2" to enabled and clickable', () => {
+    const stub = cy.stub();
+    cy.on('window:alert', stub);
+
+    cy.get('.slick-cell-menu .slick-cell-menu-option-list')
+      .find(".slick-cell-menu-item")
+      .contains("False")
+      .click();
+
+    cy.get('#myGrid')
+      .find('.slick-row .slick-cell:nth(7)')
+      .contains("Action")
+      .click();
+
+
+    cy.get('.slick-cell-menu .slick-cell-menu-item')
+      .contains("Command 2")
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('Command 2')
+      })
+  });
 });
