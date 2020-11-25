df8140ba4fccb6553fd1c2d0e8f6f53be7da2881
ghiscoding <gbeaulac@gmail.com>
2020-11-25 10:31:41 -0500

    feat: add enableMouseWheelScrollHandler option to allow dynamic load - when using TypeScript, we must always import the mousewheel jquery lib even if we don't need it because TypeScript does not yet support dynamic import. So providing a grid option to only use the mousewheel handler when we really want it is the only bulletproof way to only use that handler only when we really need it (typically when using a frozen grid) - ref Angular-Slickgrid [issue](https://github.com/ghiscoding/Angular-Slickgrid/issues/618)

--- slick.grid.js
+++ slick.grid.js
@@ -113,6 +113,7 @@ if (typeof Slick === "undefined") {
       minRowBuffer: 3,
       emulatePagingWhenScrolling: true, // when scrolling off bottom of viewport, place new row at top of viewport
       editorCellNavOnLRKeys: false,
+      enableMouseWheelScrollHandler: true,
       doPaging: true,
       autosizeColsMode: Slick.GridAutosizeColsMode.LegacyOff,
       autosizeColPaddingPx: 4,
@@ -567,7 +568,7 @@ if (typeof Slick === "undefined") {
         $viewport
             .on("scroll", handleScroll);
 
-        if (jQuery.fn.mousewheel) {
+        if (jQuery.fn.mousewheel && options.enableMouseWheelScrollHandler) {
           $viewport.on("mousewheel", handleMouseWheel);
         }
 
