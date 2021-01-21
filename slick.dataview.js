fe6572a7f87819f3d33ae61d2c36b3047883d1a4
KUSCHNY Daniel <daniel.kuschny@leica-geosystems.com>
2021-01-21 18:43:09 +0100

    Example added

--- slick.dataview.js
+++ slick.dataview.js
@@ -521,13 +521,13 @@
     }
 
     function deleteItem(id) {
-      var idx = idxById.get(id);
-      if (idx === undefined) {
-        throw new Error("Invalid id");
-      }
       if (isBulkSuspend) {
         bulkDeleteIds.set(id, true);
       } else {
+        var idx = idxById.get(id);
+        if (idx === undefined) {
+          throw new Error("Invalid id");
+        }
         idxById.delete(id);
         items.splice(idx, 1);
         updateIdxById(idx);
