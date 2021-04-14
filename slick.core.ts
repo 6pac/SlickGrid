e967c0bd7e4a68c341ca48ccf91de3da0291973c
JacobMGEvans <jacobmgevans@gmail.com>
2021-04-13 23:22:03 -0500

    Types Map - Added a Map interface - Created a more comphrensive MapConstructor interface borrowing from Lodash ES5 compatible MapConstructor - Made the types intergrated into the methods generic with the generic interfaces for better inference of types from user input

--- slick.core.ts
+++ slick.core.ts
@@ -718,57 +718,74 @@
     };
   }
 
+
+  interface IMap<K, V> {
+    clear(): void;
+    delete(key: K): void;
+    forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any): void;
+    get(key: K): V | undefined;
+    has(key: K): boolean;
+    set(key: K, value: V): void; //? Return type should probably be contextual "this"
+    readonly size: number;
+}
+  interface MapConstructor {
+    new(): Map<any, any>;
+    new<K, V>(entries?: readonly (readonly [K, V])[] | null): Map<K, V>;
+    readonly prototype: Map<any, any>;
+  }
   /***
    * Polyfill for Map to support old browsers but
    * benefit of the Map speed in modern browsers.
    * @class Map
    * @constructor
    */
-  var Map =
+   const Map = function <K extends keyof V,V>(this: IMap<K, V>) {
+    var data: Record<K, V>
+
+    /***
+     * Gets the item with the given key from the map or undefined if
+     * the map does not contain the item.
+     * @method get
+     * @param key The key of the item in the map.
+     */
+    this.get = function (key: K) {
+      return data[key];
+    };
+
+    /***
+     * Adds or updates the item with the given key in the map.
+     * @method set
+     * @param key The key of the item in the map.
+     * @param value The value to insert into the map of the item in the map.
+     */
+    this.set = function (key: K, value: V) {
+     data[key] = value;
+    };
+
+    /***
+     * Gets a value indicating whether the given key is present in the map.
+     * @method has
+     * @param key The key of the item in the map.
+     * @return {Boolean}
+     */
+    this.has = function (key: K): boolean {
+      return key in data;
+    };
+
+    /***
+     * Removes the item with the given key from the map.
+     * @method delete
+     * @param key The key of the item in the map.
+     */
+    this.delete = function (key: K) {
+      delete data[key];
+    };
+  } as any as MapConstructor
+
+  var MapPolly =
     "Map" in window
       ? window.Map
-      : function Map() {
-          var data = {};
-
-          /***
-           * Gets the item with the given key from the map or undefined if
-           * the map does not contain the item.
-           * @method get
-           * @param key {Map} The key of the item in the map.
-           */
-          this.get = function (key: Map) {
-            return data[key];
-          };
-
-          /***
-           * Adds or updates the item with the given key in the map.
-           * @method set
-           * @param key The key of the item in the map.
-           * @param value The value to insert into the map of the item in the map.
-           */
-          this.set = function (key, value) {
-            data[key] = value;
-          };
-
-          /***
-           * Gets a value indicating whether the given key is present in the map.
-           * @method has
-           * @param key The key of the item in the map.
-           * @return {Boolean}
-           */
-          this.has = function (key): boolean {
-            return key in data;
-          };
-
-          /***
-           * Removes the item with the given key from the map.
-           * @method delete
-           * @param key The key of the item in the map.
-           */
-          this.delete = function (key) {
-            delete data[key];
-          };
-        };
+      : Map
 
   // exports
   $.extend(true, window, {
@@ -777,7 +794,7 @@
       EventData: EventData,
       EventHandler: EventHandler,
       Range: Range,
-      Map: Map,
+      Map: MapPolly,
       NonDataRow: NonDataItem,
       Group: Group,
       GroupTotals: GroupTotals,
