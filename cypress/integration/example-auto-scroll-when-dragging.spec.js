d3854fda06d91c07e1f94b98055ef24d796d230c
David Hsueh <hsuehchiahao@gmail.com>
2021-11-26 17:01:37 +0800

    fix: Fix the cypress file to pass the test

--- cypress/integration/example-auto-scroll-when-dragging.spec.js
+++ cypress/integration/example-auto-scroll-when-dragging.spec.js
@@ -30,11 +30,11 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
   });
 
   function dragStart(parentClass, row, col) {
-    return cy.get(`${parentClass || ''} [style="top:${row * 25}px"] > .slick-cell:nth(${col})`, )
+    cy.get(`${parentClass || ''} [style="top:${row * 25}px"] > .slick-cell:nth(${col})`, )
       .as('dragStart')
       .click({ force: true })
-      .trigger('mousedown', { which: 1, force: true })
-      .trigger('mousemove', cellWidth/3, cellHeight/3, { force: true });
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', cellWidth/3, cellHeight/3);
   }
 
   function drag(addRow, addCell) {
@@ -47,12 +47,12 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
     if (position.toLowerCase().indexOf("left") > -1) {
       x = -px;
     } else if (position.toLowerCase().indexOf("right") > -1) {
-      x = gridWidth - scrollbarDimension + 1 + (px || 0);
+      x = gridWidth - scrollbarDimension + 3 + (px || 0);
     }
     if (position.toLowerCase().indexOf("top") > -1) {
       y = -px;
     } else if (position.toLowerCase().indexOf("bottom") > -1) {
-      y = gridHeight - scrollbarDimension + 1 + (px || 0);
+      y = gridHeight - scrollbarDimension + 3 + (px || 0);
     }
     cy.get(` ${selector || ''}`).trigger('mousemove', x, y, { force: true });
     if (ms) {
@@ -135,35 +135,40 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
 
   function getIntervalUntilRow16Displayed(selector, px) {
     dragStart(selector, 0, 1);
-    return cy.get(selector + ' .slick-viewport:first').as('viewport').invoke('scrollTop').then(scrollBefore => {
+    cy.get(selector + ' .slick-viewport:first').as('viewport').invoke('scrollTop').then(scrollBefore => {
       dragOutside(selector, 'bottom', 0, px);
+
       const start = performance.now();
       cy.get(selector + ' .slick-row:not(.slick-group) >.cell-unselectable')
         .contains('16', { timeout: 10000 }) // actually #15 will be selected
         .should('not.be.hidden');
-      return cy.get('@viewport').invoke('scrollTop').then(scrollAfter => {
+
+      cy.get('@viewport').invoke('scrollTop').then(scrollAfter => {
         dragEnd(selector);
         var interval = performance.now() - start;
         expect(scrollBefore).to.be.lessThan(scrollAfter);
-        return cy.wrap(interval);
+        cy.wrap(interval).as('interval' + selector);
       })
     })
   }
 
-  function testInterval(px) {
-    return getIntervalUntilRow16Displayed("#myGrid", px).then(intervalCell => {
-      return getIntervalUntilRow16Displayed("#myGrid2", px).then(intervalRow => {
-        return cy.wrap({
+  function testInterval(key, px) {
+    getIntervalUntilRow16Displayed("#myGrid", px);
+    getIntervalUntilRow16Displayed("#myGrid2", px);
+    cy.get('@interval#myGrid').then(intervalCell => {
+      cy.get('@interval#myGrid2').then(intervalRow => {
+        cy.wrap({
           cell: intervalCell,
           row: intervalRow
-        })
+        }).as(key)
       });
     });
   }
 
   it('should MIN interval take effect when auto scroll: 30ms -> 90ms', { scrollBehavior: false }, function () {
     // By default the MIN interval to show next cell is 30ms.
-    testInterval(300).then(defaultInterval => {
+    testInterval('default', 300);
+    cy.get('@default').then(defaultInterval => {
 
       resetScroll();
       // Setting the interval to 90ms (3 times of the default).
@@ -172,7 +177,8 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
 
       // Ideally if we scrolling to same row by MIN interval, the used time should be 3 times slower than default.
       // Considering the threshold, 1.5 times slower than default is expected
-      testInterval(300).then(newInterval => {
+      testInterval('after', 300);
+      cy.get('@after').then(newInterval => {
 
         // max scrolling speed is slower than before
         expect(newInterval.cell).to.be.greaterThan(1.5 * defaultInterval.cell);
@@ -187,7 +193,8 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
 
   it('should MAX interval take effect when auto scroll: 600ms -> 200ms', { scrollBehavior: false }, function () {
     // By default the MAX interval to show next cell is 600ms.
-    testInterval(0).then(defaultInterval => {
+    testInterval('default', 0);
+    cy.get('@default').then(defaultInterval => {
 
       resetScroll();
       // Setting the interval to 200ms (1/3 of the default).
@@ -196,7 +203,8 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
 
       // Ideally if we scrolling to same row by MAX interval, the used time should be 3 times faster than default.
       // Considering the threshold, 1.5 times faster than default is expected
-      testInterval(0).then(newInterval => {
+      testInterval('after', 0);
+      cy.get('@after').then(newInterval => {
 
         // min scrolling speed is quicker than before
         expect(1.5 * newInterval.cell).to.be.lessThan(defaultInterval.cell);
@@ -211,7 +219,8 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
 
   it('should Delay per Px take effect when auto scroll: 5ms/px -> 50ms/px', { scrollBehavior: false }, function () {
     // By default the Delay per Px is 5ms/px.
-    testInterval(scrollbarDimension).then(defaultInterval => {
+    testInterval('default', scrollbarDimension);
+    cy.get('@default').then(defaultInterval => {
 
       resetScroll();
       // Setting to 50ms/px (10 times of the default).
@@ -221,7 +230,8 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
       // Ideally if we scrolling to same row, and set cursor to 17px, the new interval will be set to MIN interval (Math.max(30, 600 - 50 * 17) = 30ms),
       // and the used time should be around 17 times faster than default.
       // Considering the threshold, 5 times faster than default is expected
-      testInterval(scrollbarDimension).then(newInterval => {
+      testInterval('after', scrollbarDimension);
+      cy.get('@after').then(newInterval => {
 
          // scrolling speed is quicker than before
         expect(5 * newInterval.cell).to.be.lessThan(defaultInterval.cell);
@@ -262,60 +272,68 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
       y = 'bottom';
     }
     dragStart(`${selector} .grid-canvas-${x}.grid-canvas-${y}`, fromRow, fromCol);
-    return cy.get(`${selector} .slick-viewport-${x}.slick-viewport-${y}`).as('viewport').then($viewport => {
+    cy.get(`${selector} .slick-viewport-${x}.slick-viewport-${y}`).as('viewport').then($viewport => {
       const scrollTopBefore = $viewport.scrollTop();
       const scrollLeftBefore = $viewport.scrollLeft();
       dragOutside(`${selector} .slick-viewport-${x}.slick-viewport-${y}`, dragDirection, 300, px || 100);
-      return cy.get('@viewport').then($viewportAfter => {
+      cy.get('@viewport').then($viewportAfter => {
         dragEnd(selector);
-        return cy.wrap({
+        cy.wrap({
           scrollTopBefore: scrollTopBefore,
           scrollLeftBefore: scrollLeftBefore,
           scrollTopAfter: $viewportAfter.scrollTop(),
           scrollLeftAfter: $viewportAfter.scrollLeft()
-        })
+        }).as('result');
       })
     })
   }
 
   it('should auto scroll to display the selecting element when dragging in frozen grid', { scrollBehavior: false }, () => {
     // top left - to bottomRight
-    dragInFrozen('#myGrid', 'topLeft', 'bottomRight', 0, 1).then(result => {
+    dragInFrozen('#myGrid', 'topLeft', 'bottomRight', 0, 1);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.equal(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.equal(result.scrollLeftAfter);
     });
-    dragInFrozen('#myGrid2', 'topLeft', 'bottomRight', 0, 1).then(result => {
+    dragInFrozen('#myGrid2', 'topLeft', 'bottomRight', 0, 1);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.equal(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.equal(result.scrollLeftAfter);
     });
 
     // top right - to bottomRight
-    dragInFrozen('#myGrid', 'topRight', 'bottomRight', 0, 0).then(result => {
+    dragInFrozen('#myGrid', 'topRight', 'bottomRight', 0, 0);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.equal(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.lessThan(result.scrollLeftAfter);
     });
-    dragInFrozen('#myGrid2', 'topRight', 'bottomRight', 0, 0).then(result => {
+    dragInFrozen('#myGrid2', 'topRight', 'bottomRight', 0, 0);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.equal(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.lessThan(result.scrollLeftAfter);
     });
     resetScroll(true);
 
     // bottom left - to bottomRight
-    dragInFrozen('#myGrid', 'bottomLeft', 'bottomRight', 0, 1).then(result => {
+    dragInFrozen('#myGrid', 'bottomLeft', 'bottomRight', 0, 1);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.lessThan(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.equal(result.scrollLeftAfter);
     });
-    dragInFrozen('#myGrid2', 'bottomLeft', 'bottomRight', 0, 1).then(result => {
+    dragInFrozen('#myGrid2', 'bottomLeft', 'bottomRight', 0, 1);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.lessThan(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.equal(result.scrollLeftAfter);
     });
 
     // bottom right - to bottomRight
-    dragInFrozen('#myGrid', 'bottomRight', 'bottomRight', 0, 0).then(result => {
+    dragInFrozen('#myGrid', 'bottomRight', 'bottomRight', 0, 0);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.lessThan(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.lessThan(result.scrollLeftAfter);
     });
-    dragInFrozen('#myGrid2', 'bottomRight', 'bottomRight', 0, 0).then(result => {
+    dragInFrozen('#myGrid2', 'bottomRight', 'bottomRight', 0, 0);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.lessThan(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.lessThan(result.scrollLeftAfter);
     });
@@ -324,11 +342,13 @@ describe('Example - Auto scroll when dragging', { retries: 1 }, () => {
     cy.get('#myGrid2 .slick-viewport-bottom.slick-viewport-right').scrollTo(cellWidth*3, cellHeight*3);
 
     // bottom right - to topLeft
-    dragInFrozen('#myGrid', 'bottomRight', 'topLeft', 8, 4, 100).then(result => {
+    dragInFrozen('#myGrid', 'bottomRight', 'topLeft', 8, 4, 100);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.greaterThan(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.greaterThan(result.scrollLeftAfter);
     });
-    dragInFrozen('#myGrid2', 'bottomRight', 'topLeft', 8, 4, 100).then(result => {
+    dragInFrozen('#myGrid2', 'bottomRight', 'topLeft', 8, 4, 100);
+    cy.get('@result').then(result => {
       expect(result.scrollTopBefore).to.be.greaterThan(result.scrollTopAfter);
       expect(result.scrollLeftBefore).to.be.greaterThan(result.scrollLeftAfter);
     });
