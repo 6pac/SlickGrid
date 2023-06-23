33385c2b3ef8dec35b4d1a8642ae72f9801a178d
ghiscoding <gbeaulac@gmail.com>
2023-06-23 19:52:06 -0400

    chore(build): move all JS and SASS files into src folder

--- scripts/builds.mjs
+++ scripts/builds.mjs
@@ -1,27 +1,40 @@
 import { build } from 'esbuild';
 import { execSync } from 'child_process';
+import copyfiles from 'copyfiles';
 import fs from 'fs-extra';
 import { globSync } from 'glob';
 import path from 'node:path';
 import { compile as sassCompile } from 'sass';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-const argv = yargs(hideBin(process.argv)).argv;
 
 import { removeImportsPlugin } from './esbuild-plugins.mjs';
 
-export const BUILD_FORMATS = ['cjs', 'esm' /*, 'iife'*/];
+const argv = yargs(hideBin(process.argv)).argv;
+export const BUILD_FORMATS = ['cjs', 'esm'];
 
 if (argv.prod) {
   executeFullBuild();
+  copySassFiles();
 }
 if (argv.test) {
   buildAllIifeFiles(getAllJSFiles());
   executeCjsEsmBuilds();
+  copySassFiles();
+}
+
+function copySassFiles() {
+  copyfiles(
+    ['src/styles/scss/*.scss', 'dist/styles/sass'],
+    { flat: true },
+    () => {
+      console.log('SASS files copied, proceeding to build all files');
+    }
+  );
 }
 
 function getAllJSFiles() {
-  return globSync(['*.{js,ts}', 'controls/**/*.{js,ts}', 'plugins/**/*.{js,ts}']);
+  return globSync(['src/**/*.{js,ts}']);
 }
 
 export async function executeFullBuild() {
@@ -60,7 +73,7 @@ export async function executeCjsEsmBuilds() {
 // bundle by either CJS or ESM formats
 export function bundleByFormat(format) {
   runBuild({
-    entryPoints: ['index.js'],
+    entryPoints: ['src/index.js'],
     format,
     target: 'es2020',
     treeShaking: true,
@@ -74,7 +87,7 @@ async function buildAllIifeFiles(allFiles) {
   const startTime = new Date().getTime();
 
   for (const file of allFiles) {
-    if (file === 'index.js' || file === 'index.ts' || file.includes('.d.ts')) {
+    if (/index.[j|t]s/i.test(file) || file.includes('.d.ts')) {
       continue; // skip index.js and any *.d.ts files which are not useful for iife
     }
     buildIifeFile(file);
@@ -86,7 +99,7 @@ async function buildAllIifeFiles(allFiles) {
 /** build as iife, every file will be bundled separately */
 export async function buildIifeFile(file) {
   let globalName;
-  if (/slick.core.[jt]s/.test(file)) {
+  if (/slick.core.[jt]s/gi.test(file)) {
     globalName = 'Slick';
   }
 
@@ -95,7 +108,7 @@ export async function buildIifeFile(file) {
     format: 'iife',
     globalName,
     define: { IIFE_ONLY: 'true' },
-    outfile: `dist/browser/${file.replace(/.[j|t]s/, '')}.js`,
+    outfile: `dist/browser/${file.replace('src', '').replace(/.[j|t]s/, '')}.js`,
     plugins: [
       removeImportsPlugin,
     ],
@@ -145,15 +158,14 @@ export function buildSassFile(sassFile) {
     buildAllSassFiles();
     console.log('prefixed scss file changed', sassFile)
   } else {
-    const srcDir = ''; // 'src'
-    const distDir = ''; // 'dist'
+    const srcDir = 'src';
+    const distDir = 'dist';
     const basePath = path.join(process.cwd(), `/${srcDir}/styles`);
     const absoluteFilePath = path.relative(basePath, sassFile);
     const posixPath = absoluteFilePath.replaceAll('\\', '/');
 
     try {
       fs.outputFileSync(
-        // `${distDir ? distDir + '/' : ''}/styles/css/${posixPath.replace('.scss', '')}.css`,
         `${distDir ? distDir + '/' : ''}styles/css/${filename}.css`,
         sassCompile(`${srcDir ? srcDir + '/' : ''}styles/${posixPath}`, { style: 'compressed', quietDeps: true, noSourceMap: true }).css
       );
