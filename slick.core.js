b31a797b8e1f25bd53a30b501ce20b2380e6d9f9
Ghislain B <gbeaulac@gmail.com>
2023-06-29 19:39:47 -0400

    fix: make sure DOM element is valid before calling method on it (#801)
    - similar to the other PR I've opened but in a different method, we need to make sure that we have a valid DOM element before calling any method on that said element. In this case, the `parents()` function was throwing when trying to call `el.matches(...)` because the element was invalid

--- slick.core.js
+++ slick.core.js
@@ -696,6 +696,9 @@
     const hidden = selector == ":hidden";
 
     while ((el = el.parentNode) && el !== document) {
+      if (!el || !el.parentNode) {
+        break;
+      }
       if (hidden) {
         if(isHidden(el)) {
           parents.push(el);
