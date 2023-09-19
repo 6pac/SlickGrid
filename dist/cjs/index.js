04b86dcffdbd41a12916113419684eeee4fd5a23
ghiscoding <gbeaulac@gmail.com>
2023-09-19 12:38:19 -0400

    chore(release): publish version 5.0.0-alpha.3

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -6220,7 +6220,7 @@ var SlickGrid = class {
     this.options = options;
     //////////////////////////////////////////////////////////////////////////////////////////////
     // Public API
-    __publicField(this, "slickGridVersion", "5.0.0-alpha.2");
+    __publicField(this, "slickGridVersion", "5.0.0-alpha.3");
     /** optional grid state clientId */
     __publicField(this, "cid", "");
     // Events
@@ -9309,7 +9309,7 @@ var SlickRemoteModel = class {
  * Distributed under MIT license.
  * All rights reserved.
  *
- * SlickGrid v5.0.0-alpha.2
+ * SlickGrid v5.0.0-alpha.3
  *
  * NOTES:
  *     Cell/row DOM manipulations are done directly bypassing JS DOM manipulation methods.
