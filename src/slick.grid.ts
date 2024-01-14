47c56f3c03b80bfc9bc811d214a1cbb524021a3f
ghiscoding <gbeaulac@gmail.com>
2024-01-14 17:02:56 -0500

    chore: fix function name typo

--- src/slick.grid.ts
+++ src/slick.grid.ts
@@ -1875,7 +1875,7 @@ export class SlickGrid<TData = any, C extends Column<TData> = Column<TData>, O e
     return a.concat(b) as HTMLElement[];
   }
 
-  protected handleResizeableHandleDoubleClick(evt: MouseEvent & { target: HTMLDivElement; }) {
+  protected handleResizeableDoubleClick(evt: MouseEvent & { target: HTMLDivElement; }) {
     const triggeredByColumn = evt.target.parentElement!.id.replace(this.uid, '');
     this.trigger(this.onColumnsResizeDblClick, { triggeredByColumn });
   }
@@ -1928,7 +1928,7 @@ export class SlickGrid<TData = any, C extends Column<TData> = Column<TData>, O e
       }
 
       const resizeableHandle = Utils.createDomElement('div', { className: 'slick-resizable-handle', role: 'separator', ariaOrientation: 'horizontal' }, colElm);
-      this._bindingEventService.bind(resizeableHandle, 'dblclick', this.handleResizeableHandleDoubleClick.bind(this) as EventListener);
+      this._bindingEventService.bind(resizeableHandle, 'dblclick', this.handleResizeableDoubleClick.bind(this) as EventListener);
 
       this.slickResizableInstances.push(
         Resizable({
