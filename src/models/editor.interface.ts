4682d24a5f99a7301ac24410279052cc583db1b8
Александр Чазов <chazovby@gmail.com>
2024-04-01 18:09:11 +0300

    fix: Editor.keyCaptureList is an array of numbers

--- src/models/editor.interface.ts
+++ src/models/editor.interface.ts
@@ -10,7 +10,7 @@ export interface Editor {
   /** is the Editor disabled when we first open it? This could happen when we use "collectionAsync" and we wait for the "collection" to be filled before enabling the Editor. */
   disabled?: boolean;
 
-  keyCaptureList?: string;
+  keyCaptureList?: number[];
 
   /** Initialize the Editor */
   init: (args?: EditorArguments) => void;
@@ -75,7 +75,7 @@ export interface Editor {
 
   /** Update the Editor DOM element value with a provided value, we can optionally apply the value to the item dataContext object */
   setValue?: (value: any, isApplyingValue?: boolean, triggerOnCompositeEditorChange?: boolean) => void;
-  
+
   /**
    * Validate user input and return the result along with the validation message.
    * if the input is valid then the validation result output would be returning { valid:true, msg:null }
