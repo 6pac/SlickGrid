a5807c754bd8d9b239566b655923ffa860fac45a
PierreYvesR <py.rivaille@gmail.com>
2024-04-08 20:27:32 +0100

    Test: SlickCellExternalCopyManager plugin restores focus on paste - check cell value

--- cypress/e2e/example-excel-compatible-spreadsheet.cy.ts
+++ cypress/e2e/example-excel-compatible-spreadsheet.cy.ts
@@ -1,10 +1,5 @@
 describe('Example - Excel-compatible spreadsheet and Cell Selection', { retries: 0 }, () => {
   const cellHeight = 25;
-  const titles = [
-    '', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-    'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK'
-  ];
 
   it('should load Example', () => {
     cy.visit(`${Cypress.config('baseUrl')}/examples/example-excel-compatible-spreadsheet.html`);
@@ -29,5 +24,8 @@ describe('Example - Excel-compatible spreadsheet and Cell Selection', { retries:
 
     cy.get('#myGrid .slick-cell.l3.r3.selected')
       .should('have.length', 1);
+
+    cy.getCell(3, 2, '', { parentSelector: "#myGrid", rowHeight: cellHeight })
+      .should('have.text', '3');
   });
 });
