e144a3cc69e82f986783820d55a8494f1710a9df
ghiscoding <gbeaulac@gmail.com>
2024-08-23 00:50:56 -0400

    docs: add note about possible transform default in next major

--- src/models/gridOption.interface.ts
+++ src/models/gridOption.interface.ts
@@ -287,7 +287,8 @@ export interface GridOption<C extends BaseColumn = BaseColumn> {
 
   /**
    * Defaults to "top", what CSS style to we want to use to render each row top offset (we can use "top" or "transform").
-   * For example, with a default `rowHeight: 22`, the 2nd row will have a `top` offset of 44px and by default have a CSS style of `top: 44px`
+   * For example, with a default `rowHeight: 22`, the 2nd row will have a `top` offset of 44px and by default have a CSS style of `top: 44px`.
+   * NOTE: for perf reasons, the "transform" might become the default in our future major version.
    */
   rowTopOffsetRenderType?: 'top' | 'transform';
 
