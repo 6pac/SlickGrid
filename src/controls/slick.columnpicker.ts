9fcadbafa58f6f02c815f76385391a7240e025b8
Ghislain B. <gbeaulac@gmail.com>
2024-09-07 14:04:28 -0400

    fix: typo in `setColumnVisibiliy` (#1057)
    - keep previous method around to avoid breaking users but deprecate it

--- src/controls/slick.columnpicker.ts
+++ src/controls/slick.columnpicker.ts
@@ -301,7 +301,12 @@ export class SlickColumnPicker {
     }
   }
 
+  /** @deprecated because of a typo @use `setColumnVisibility()` instead */
   setColumnVisibiliy(idxOrId: number | string, show: boolean) {
+    this.setColumnVisibility(idxOrId, show);
+  }
+
+  setColumnVisibility(idxOrId: number | string, show: boolean) {
     const idx = typeof idxOrId === 'number' ? idxOrId : this.getColumnIndexbyId(idxOrId);
     let visibleColumns = this.getVisibleColumns();
     const col = this.columns[idx];
