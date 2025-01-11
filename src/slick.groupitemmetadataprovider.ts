7a68cc903b3e64ab9e84e894b5f1b1ef563b614d
ghiscoding <gbeaulac@gmail.com>
2025-01-11 16:01:09 -0500

    feat: add Custom Global Item Data Provider, closes #262

--- src/slick.groupitemmetadataprovider.ts
+++ src/slick.groupitemmetadataprovider.ts
@@ -164,7 +164,7 @@ export class SlickGroupItemMetadataProvider implements SlickPlugin {
     }
   }
 
-  getGroupRowMetadata(item: GroupingFormatterItem): ItemMetadata {
+  getGroupRowMetadata(item: GroupingFormatterItem, _row: number): ItemMetadata {
     const groupLevel = item?.level;
     return {
       selectable: false,
@@ -181,7 +181,7 @@ export class SlickGroupItemMetadataProvider implements SlickPlugin {
     };
   }
 
-  getTotalsRowMetadata(item: { group: GroupingFormatterItem }): ItemMetadata | null {
+  getTotalsRowMetadata(item: { group: GroupingFormatterItem }, _row: number): ItemMetadata | null {
     const groupLevel = item?.group?.level;
     return {
       selectable: false,
