2418edc57feafdfa8f6067c26a29a9b24a141b5c
tritontrident <tritontrident@noemail.com>
2025-11-16 18:27:22 +0100

    fix: unable to drag row when the cell formatter renders html elements

--- src/slick.grid.ts
+++ src/slick.grid.ts
@@ -979,7 +979,7 @@ export class SlickGrid<TData = any, C extends Column<TData> = Column<TData>, O e
       if (Draggable) {
         this.slickDraggableInstance = Draggable({
           containerElement: this._container,
-          allowDragFrom: 'div.slick-cell, div.' + this.dragReplaceEl.cssClass,
+          allowDragFrom: 'div.slick-cell, div.slick-cell *, div.' + this.dragReplaceEl.cssClass,
           dragFromClassDetectArr: [{ tag: 'dragReplaceHandle', id: this.dragReplaceEl.id }],
           // the slick cell parent must always contain `.dnd` and/or `.cell-reorder` class to be identified as draggable
           allowDragFromClosest: 'div.slick-cell.dnd, div.slick-cell.cell-reorder',
