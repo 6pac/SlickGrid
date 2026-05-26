e34bb0a7d0021b95cf0b4278cb4d18f466208eff
Ghislain B. <gbeaulac@gmail.com>
2026-05-26 00:01:33 -0400

    chore(deps): upgrade to ESLint v10 (#1215)
    * chore(deps): upgrade to ESLint v10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,14 @@ import eslint from '@eslint/js';
 import cypress from 'eslint-plugin-cypress';
 import globals from 'globals';
 import n from 'eslint-plugin-n';
-import tseslint from 'typescript-eslint';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
+export default [
+  // Base JS recommended
   eslint.configs.recommended,
-  // ...cypress.configs.recommended,
-  ...tseslint.configs.recommended,
+
+  // Ignore globs
   {
     ignores: [
       '**/*.css',
@@ -31,27 +33,30 @@ export default tseslint.config(
       '**/*.d.ts',
     ],
   },
-  {
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      cypress,
-      n
-    },
-    files: ['**/*.ts'],
 
+  // TypeScript files
+  {
+    files: ['**/*.ts'],
     languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.json', 'cypress/tsconfig.json']
+      },
       globals: {
         ...globals.es2021,
         ...globals.node,
-        flatpickr: true,
-        Slick: true,
-        Sortable: true,
-        IIFE_ONLY: true
-      },
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ['./tsconfig.json', 'cypress/tsconfig.json']
+        ...globals.browser,
+        flatpickr: 'readonly',
+        moment: 'readonly',
+        Slick: 'readonly',
+        Sortable: 'readonly',
+        IIFE_ONLY: 'readonly'
       }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      cypress,
+      n
     },
     settings: {
       node: {
@@ -63,31 +68,53 @@ export default tseslint.config(
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/consistent-type-exports': 'error',
       '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-empty-object-type': [
-        'error',
-        { allowInterfaces: 'with-single-extends' }, // maybe we should turn this on in a new PR
-      ],
+      '@typescript-eslint/no-empty-object-type': 'error',
       '@typescript-eslint/no-empty-function': 'off',
       '@typescript-eslint/no-unsafe-function-type': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-inferrable-types': 'error',
       '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_', 'destructuredArrayIgnorePattern': '^_', caughtErrors: 'none' }],
-      'curly': 'error',
-      'cypress/no-assigning-return-values': 'off',
-      'cypress/no-unnecessary-waiting': 'off',
-      'cypress/unsafe-to-chain-command': 'off',
-      'eqeqeq': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_', caughtErrors: 'none' }],
+
+      // core rules
+      curly: 'error',
+      eqeqeq: 'error',
       'object-shorthand': 'error',
       'no-async-promise-executor': 'off',
       'no-case-declarations': 'off',
       'no-cond-assign': 'off',
-      'no-prototype-builtins': [0],
+      'no-unused-vars': 'off',
+      'no-useless-assignment': 'off',
+      'no-prototype-builtins': 'off',
       'no-extra-boolean-cast': 'off',
-      'n/file-extension-in-import': ['error', 'always', { ".cy.ts": "never" }],
-      'semi': 'off',
+      semi: 'off',
       'keyword-spacing': 'error',
       'space-before-blocks': 'error',
+
+      // Cypress plugin rules
+      'cypress/no-assigning-return-values': 'off',
+      'cypress/no-unnecessary-waiting': 'off',
+      'cypress/unsafe-to-chain-command': 'off',
+
+      // eslint-plugin-n
+      'n/file-extension-in-import': ['error', 'always', { '.cy.ts': 'never' }]
     }
-  });
+  },
+
+  // Cypress/test override
+  {
+    files: ['cypress/**/*.ts', '**/*.cy.ts', '**/*.spec.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { project: ['./cypress/tsconfig.json'] },
+      globals: { ...globals.es2021, ...globals.node }
+    },
+    plugins: { cypress },
+    rules: {
+      'cypress/no-assigning-return-values': 'off',
+      'cypress/no-unnecessary-waiting': 'off',
+      'cypress/unsafe-to-chain-command': 'off'
+    }
+  }
+];
